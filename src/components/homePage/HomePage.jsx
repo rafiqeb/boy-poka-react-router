b89a0ea0e68ddbe8154987ac292d1ac68a27d1b4
rafiqeb <mdrafiqulislameb@gmail.com>
2024-10-31 21:34:16 +0600

    first commit

---
+++ src/components/homePage/HomePage.jsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">this is home page</h2>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/root/Root';
+import ErrorPage from './components/errorPage/ErrorPage';
+import HomePage from './components/homePage/HomePage';
+import Dashboard from './components/dashboard/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
